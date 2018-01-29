84aa1c9b5eaeb21bfc3c08d40a0f5fd84bd4ca8c
YSRKEN <ysr.ken@gmail.com>
2018-01-29 21:38:33 +0900

    SQLiteを導入した

---
+++ AWSK/Stores/DataStore.cs
@@ -0,0 +1,60 @@
+﻿using System.Data.SQLite;
+
+namespace AWSK.Stores
+{
+	static class DataStore
+	{
+		private const string connectionString = @"Data Source=GameData.db";
+		public static void Initialize() {
+			// テーブルが存在しない場合、テーブルを作成する
+			using (var con = new SQLiteConnection(connectionString)) {
+				con.Open();
+				using (var cmd = con.CreateCommand()) {
+					// 艦娘データ
+					{
+						bool hasTableFlg = false;
+						cmd.CommandText = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='Kammusu'";
+						using (var reader = cmd.ExecuteReader()) {
+							if (reader.Read() && reader.GetInt32(0) == 1) {
+								hasTableFlg = true;
+							}
+						}
+						if (!hasTableFlg) {
+							string sql = @"
+							CREATE TABLE [Kammusu](
+							[id] INTEGER NOT NULL UNIQUE,
+							[name] TEXT NOT NULL DEFAULT '',
+							PRIMARY KEY(id))
+						";
+							cmd.CommandText = sql;
+							cmd.ExecuteNonQuery();
+						}
+					}
+					// 装備データ
+					{
+						bool hasTableFlg = false;
+						cmd.CommandText = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='Weapon'";
+						using (var reader = cmd.ExecuteReader()) {
+							if (reader.Read() && reader.GetInt32(0) == 1) {
+								hasTableFlg = true;
+							}
+						}
+						if (!hasTableFlg) {
+							string sql = @"
+							CREATE TABLE [Weapon](
+							[id] INTEGER NOT NULL UNIQUE,
+							[name] TEXT NOT NULL DEFAULT '',
+							PRIMARY KEY(id))
+						";
+							cmd.CommandText = sql;
+							cmd.ExecuteNonQuery();
+						}
+					}
+				}
+			}
+		}
+		public static bool DownloadData() {
+			return true;
+		}
+	}
+}
--- AWSK/ViewModels/MainViewModel.cs
+++ AWSK/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Reactive.Bindings;
+﻿using AWSK.Stores;
+using Reactive.Bindings;
 using System;
 using System.Windows;
 
@@ -13,6 +14,8 @@ namespace AWSK.ViewModels
 
 		// コンストラクタ
 		public MainViewModel() {
+			DataStore.Initialize();
+			DataStore.DownloadData();
 			ImportClipboardTextCommand.Subscribe(_ => MessageBox.Show("スタブ", "AWSK"));
 		}
 	}
