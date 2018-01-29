24ace24de2015d0826b90400f7a0da2d26975610
YSRKEN <ysr.ken@gmail.com>
2018-01-30 00:57:10 +0900

    ParseFleetDataメソッドの位置を変更した

--- AWSK/Stores/DataStore.cs
+++ AWSK/Stores/DataStore.cs
@@ -230,6 +230,69 @@ namespace AWSK.Stores
 			}
 			return wd;
 		}
+		// JSONデータを分析する
+		public static FleetData ParseFleetData(string jsonString) {
+			var obj = DynamicJson.Parse(jsonString);
+			var fleetData = new FleetData();
+			for (int fi = 1; fi <= 4; ++fi) {
+				// 定義されていない艦隊は飛ばす
+				if (!obj.IsDefined($"f{fi}"))
+					continue;
+				var kammusuList = new List<KammusuData>();
+				for (int si = 1; si <= 6; ++si) {
+					// 定義されていない艦隊は飛ばす
+					if (!obj[$"f{fi}"].IsDefined($"s{si}"))
+						continue;
+					// 中身が定義されていないようなら飛ばす
+					if (!obj[$"f{fi}"][$"s{si}"].IsDefined("id"))
+						continue;
+					// idを読み取り、そこから艦名を出力
+					int k_id = int.Parse(obj[$"f{fi}"][$"s{si}"].id);
+					int lv = (int)(obj[$"f{fi}"][$"s{si}"].lv);
+					var kammusuData = DataStore.KammusuDataById(k_id);
+					kammusuData.Level = lv;
+					for (int ii = 1; ii <= 5; ++ii) {
+						string key = (ii == 5 ? "ix" : $"i{ii}");
+						// 定義されていない装備は飛ばす
+						if (!obj[$"f{fi}"][$"s{si}"].items.IsDefined(key))
+							continue;
+						// idを読み取り、そこから装備名を出力
+						int w_id = (int)(obj[$"f{fi}"][$"s{si}"].items[key].id);
+						var weaponData = DataStore.WeaponDataById(w_id);
+						// 艦載機熟練度を出力
+						// 「装備改修度が0なら『0』、1以上なら『"1"』」といった
+						// 恐ろしい仕様があるので対策が面倒だった
+						if (obj[$"f{fi}"][$"s{si}"].items[key].IsDefined("mas")) {
+							var rawMas = obj[$"f{fi}"][$"s{si}"].items[key].mas;
+							int mas = 0;
+							if (rawMas.GetType() == typeof(string)) {
+								mas = int.Parse(rawMas);
+							} else {
+								mas = (int)(rawMas);
+							}
+							weaponData.Mas = mas;
+						}
+						//
+						// 装備改修度を出力
+						// ヤバさは艦載機熟練度と同様
+						if (obj[$"f{fi}"][$"s{si}"].items[key].IsDefined("rf")) {
+							var rawRf = obj[$"f{fi}"][$"s{si}"].items[key].rf;
+							int rf = 0;
+							if (rawRf.GetType() == typeof(string)) {
+								rf = int.Parse(rawRf);
+							} else {
+								rf = (int)(rawRf);
+							}
+							weaponData.Rf = rf;
+						}
+						kammusuData.Weapon.Add(weaponData);
+					}
+					kammusuList.Add(kammusuData);
+				}
+				fleetData.Kammusu.Add(kammusuList);
+			}
+			return fleetData;
+		}
 	}
 	// 艦隊データ
 	class FleetData
--- AWSK/ViewModels/MainViewModel.cs
+++ AWSK/ViewModels/MainViewModel.cs
@@ -1,8 +1,6 @@
 ﻿using AWSK.Stores;
-using Codeplex.Data;
 using Reactive.Bindings;
 using System;
-using System.Collections.Generic;
 using System.Windows;
 
 namespace AWSK.ViewModels
@@ -14,69 +12,6 @@ namespace AWSK.ViewModels
 		public ReactiveCommand ImportClipboardTextCommand { get; } = new ReactiveCommand();
 		#endregion
 
-		// JSONデータを分析する
-		private FleetData ParseFleetData(dynamic obj) {
-			var fleetData = new FleetData();
-			for (int fi = 1; fi <= 4; ++fi) {
-				// 定義されていない艦隊は飛ばす
-				if (!obj.IsDefined($"f{fi}"))
-					continue;
-				var kammusuList = new List<KammusuData>();
-				for (int si = 1; si <= 6; ++si) {
-					// 定義されていない艦隊は飛ばす
-					if (!obj[$"f{fi}"].IsDefined($"s{si}"))
-						continue;
-					// 中身が定義されていないようなら飛ばす
-					if (!obj[$"f{fi}"][$"s{si}"].IsDefined("id"))
-						continue;
-					// idを読み取り、そこから艦名を出力
-					int k_id = int.Parse(obj[$"f{fi}"][$"s{si}"].id);
-					int lv = (int)(obj[$"f{fi}"][$"s{si}"].lv);
-					var kammusuData = DataStore.KammusuDataById(k_id);
-					kammusuData.Level = lv;
-					for (int ii = 1; ii <= 5; ++ii) {
-						string key = (ii == 5 ? "ix" : $"i{ii}");
-						// 定義されていない装備は飛ばす
-						if (!obj[$"f{fi}"][$"s{si}"].items.IsDefined(key))
-							continue;
-						// idを読み取り、そこから装備名を出力
-						int w_id = (int)(obj[$"f{fi}"][$"s{si}"].items[key].id);
-						var weaponData = DataStore.WeaponDataById(w_id);
-						// 艦載機熟練度を出力
-						// 「装備改修度が0なら『0』、1以上なら『"1"』」といった
-						// 恐ろしい仕様があるので対策が面倒だった
-						if (obj[$"f{fi}"][$"s{si}"].items[key].IsDefined("mas")) {
-							var rawMas = obj[$"f{fi}"][$"s{si}"].items[key].mas;
-							int mas = 0;
-							if (rawMas.GetType() == typeof(string)) {
-								mas = int.Parse(rawMas);
-							} else {
-								mas = (int)(rawMas);
-							}
-							weaponData.Mas = mas;
-						}
-						//
-						// 装備改修度を出力
-						// ヤバさは艦載機熟練度と同様
-						if (obj[$"f{fi}"][$"s{si}"].items[key].IsDefined("rf")) {
-							var rawRf = obj[$"f{fi}"][$"s{si}"].items[key].rf;
-							int rf = 0;
-							if(rawRf.GetType() == typeof(string)) {
-								rf = int.Parse(rawRf);
-							} else {
-								rf = (int)(rawRf);
-							}
-							weaponData.Rf =rf;
-						}
-						kammusuData.Weapon.Add(weaponData);
-					}
-					kammusuList.Add(kammusuData);
-				}
-				fleetData.Kammusu.Add(kammusuList);
-			}
-			return fleetData;
-		}
-
 		// クリップボードからインポート
 		public void ImportClipboardText() {
 			try {
@@ -85,8 +20,7 @@ namespace AWSK.ViewModels
 				if (clipboardString == null)
 					throw new Exception();
 				// クリップボードの文字列をJSONとしてパースし、艦隊データに変換する
-				var obj = DynamicJson.Parse(clipboardString);
-				var fleetData = ParseFleetData(obj);
+				var fleetData = DataStore.ParseFleetData(clipboardString);
 				MessageBox.Show(fleetData.ToString(), "AWSK");
 			} catch (Exception e) {
 				Console.WriteLine(e.ToString());
