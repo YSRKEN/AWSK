b458731ecc688d4509b500121633761416d33cb5
YSRKEN <ysr.ken@gmail.com>
2018-01-31 00:14:27 +0900

    各オブジェクトにプロパティを貼り付けていく

--- AWSK/ViewModels/MainViewModel.cs
+++ AWSK/ViewModels/MainViewModel.cs
@@ -10,11 +10,61 @@ namespace AWSK.ViewModels
 		#region プロパティ
 		// データベースからデータを拾ったか？
 		public ReactiveProperty<bool> ReadyDataStoreFlg = new ReactiveProperty<bool>(false);
+		// 基地航空隊を飛ばしたか？
+		public ReactiveProperty<bool> BasedAirUnit1Flg = new ReactiveProperty<bool>(false);
+		public ReactiveProperty<bool> BasedAirUnit2Flg = new ReactiveProperty<bool>(false);
+		public ReactiveProperty<bool> BasedAirUnit3Flg = new ReactiveProperty<bool>(false);
+		// 基地航空隊の装備の選択番号
+		public ReactiveProperty<int> BasedAirUnitIndex11 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitIndex12 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitIndex13 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitIndex14 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitIndex21 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitIndex22 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitIndex23 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitIndex24 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitIndex31 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitIndex32 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitIndex33 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitIndex34 = new ReactiveProperty<int>(0);
+		// 基地航空隊の艦載機熟練度
+		public ReactiveProperty<int> BasedAirUnitMas11 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitMas12 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitMas13 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitMas14 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitMas21 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitMas22 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitMas23 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitMas24 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitMas31 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitMas32 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitMas33 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitMas34 = new ReactiveProperty<int>(0);
+		// 基地航空隊の装備改修度
+		public ReactiveProperty<int> BasedAirUnitRf11 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitRf12 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitRf13 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitRf14 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitRf21 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitRf22 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitRf23 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitRf24 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitRf31 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitRf32 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitRf33 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> BasedAirUnitRf34 = new ReactiveProperty<int>(0);
+		// 敵艦隊の艦の選択番号
+		public ReactiveProperty<int> EnemyUnitIndex1 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> EnemyUnitIndex2 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> EnemyUnitIndex3 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> EnemyUnitIndex4 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> EnemyUnitIndex5 = new ReactiveProperty<int>(0);
+		public ReactiveProperty<int> EnemyUnitIndex6 = new ReactiveProperty<int>(0);
 		#endregion
 
 		#region コマンド
-		// クリップボードからインポート
-		public ReactiveCommand ImportClipboardTextCommand { get; }
+		// シミュレーションを実行
+		public ReactiveCommand RunSimulationCommand { get; } = new ReactiveCommand();
 		#endregion
 
 		// その他初期化用コード
@@ -59,7 +109,7 @@ namespace AWSK.ViewModels
 		// コンストラクタ
 		public MainViewModel() {
 			// プロパティ・コマンドを設定
-
+			RunSimulationCommand.Subscribe(ImportClipboardText);	//スタブ
 			// その他初期化
 			Initialize();
 		}
