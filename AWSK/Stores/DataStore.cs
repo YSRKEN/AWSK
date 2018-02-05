66d597b4c928898e7403c3bc9a4d5a105e3348ae
YSRKEN <ysr.ken@gmail.com>
2018-02-05 18:44:31 +0900

    基地航空隊の読み込み・書き出しに対応した

--- AWSK/Stores/DataStore.cs
+++ AWSK/Stores/DataStore.cs
@@ -717,6 +717,47 @@ namespace AWSK.Stores
 			}
 			return slotData;
 		}
+		// 情報をJsonで書き出す
+		public string GetJsonData() {
+			if (Weapon.Count == 0)
+				return "[]";
+			string output = "[";
+			for(int ui = 0; ui < Weapon.Count; ++ui) {
+				if (ui != 0)
+					output += ",";
+				output += "{";
+				output += $"\"count\":{SallyCount[ui]},\"weapon\":[";
+				for(int wi = 0; wi < Weapon[ui].Count; ++wi) {
+					if (wi != 0)
+						output += ",";
+					output += "{";
+					var w = Weapon[ui][wi];
+					output += $"\"id\":{w.Id},\"mas\":{w.Mas},\"rf\":{w.Rf}";
+					output += "}";
+				}
+				output += "]}";
+			}
+			output += "]";
+			return output;
+		}
+		// コンストラクタ
+		public BasedAirUnitData() { }
+		public BasedAirUnitData(string jsonString) {
+			// JSONをパース
+			var obj = DynamicJson.Parse(jsonString);
+			// パース結果を翻訳する
+			foreach(var weaponList in obj) {
+				SallyCount.Add((int)weaponList.count);
+				var list = new List<WeaponData>();
+				foreach(var weapon in weaponList.weapon) {
+					WeaponData weaponData = DataStore.WeaponDataById((int)weapon.id);
+					weaponData.Mas = (int)weapon.mas;
+					weaponData.Rf = (int)weapon.rf;
+					list.Add(weaponData);
+				}
+				Weapon.Add(list);
+			}
+		}
 	}
 	// 艦娘データ
 	struct KammusuData
--- AWSK/ViewModels/MainViewModel.cs
+++ AWSK/ViewModels/MainViewModel.cs
@@ -90,7 +90,6 @@ namespace AWSK.ViewModels
 		// 基地航空隊に使用できる装備の一覧
 		public ReadOnlyReactiveCollection<string> BasedAirUnitList { get; }
 		#endregion
-
 		#region コマンド
 		// シミュレーションを実行
 		public ReactiveCommand RunSimulationCommand { get; }
@@ -102,24 +101,12 @@ namespace AWSK.ViewModels
 		public ReactiveCommand LoadEnemyUnitCommand { get; } = new ReactiveCommand();
 		// 右クリックから敵編成を保存
 		public ReactiveCommand SaveEnemyUnitCommand { get; } = new ReactiveCommand();
+		// 右クリックから基地航空隊を読み込み
+		public ReactiveCommand LoadBasedAirUnitCommand { get; } = new ReactiveCommand();
+		// 右クリックから基地航空隊を保存
+		public ReactiveCommand SaveBasedAirUnitCommand { get; } = new ReactiveCommand();
 		#endregion
-
-		// その他初期化用コード
-		private async void Initialize() {
-			// データベースの初期化
-			var status = await DataStore.Initialize();
-			switch (status) {
-			case DataStoreStatus.Exist:
-				break;
-			case DataStoreStatus.Success:
-				MessageBox.Show("ダウンロードに成功しました。", "AWSK");
-				break;
-			case DataStoreStatus.Failed:
-				MessageBox.Show("ダウンロードに失敗しました。", "AWSK");
-				CloseWindow.Value = true;
-				return;
-			}
-		}
+		#region メソッド(基地航空隊用)
 		// 基地航空隊のデータを取得
 		private BasedAirUnitData GetBasedAirUnitData() {
 			// 準備
@@ -168,46 +155,11 @@ namespace AWSK.ViewModels
 			}
 			return basedAirUnitData;
 		}
-		// 敵艦隊のデータを取得
-		private FleetData GetEnemyData() {
-			var fleetData = new FleetData();
-			// 艦隊データを格納するインスタンスを作成
-			var kammusuList = new List<KammusuData>();
-			// 準備
-			var enemyUnitIndex = new[] {
-				EnemyUnitIndex1.Value, EnemyUnitIndex2.Value, EnemyUnitIndex3.Value,
-				EnemyUnitIndex4.Value, EnemyUnitIndex5.Value, EnemyUnitIndex6.Value,
-				};
-			// 作成
-			foreach(int enemyIndex in enemyUnitIndex) {
-				// 「なし」が選択されている敵艦は無視する
-				if (enemyIndex == 0)
-					continue;
-				// idを算出する
-				int enemyId = enemyIdList[enemyIndex];
-				// idから敵艦情報を得る
-				var enemy = DataStore.KammusuDataById(enemyId, true);
-				// 追加
-				kammusuList.Add(enemy);
-			}
-			// インスタンスを代入
-			if (kammusuList.Count > 0)
-				fleetData.Kammusu.Add(kammusuList);
-			return fleetData;
-		}
 		// 装備名から、基地航空隊の中隊を選択する(デバッグ用)
 		// LINQにおけるIndexの取り方：
 		// http://shirakamisauto.hatenablog.com/entry/2016/06/27/080017
 		private int BAUIndex(string name) {
 			return BasedAirUnitList
-				.Select((c, i) => new { Content = c, Index = i })
-				.Where(pair => pair.Content == name)
-				.Select(pair => pair.Index)
-				.First();
-		}
-		// 艦名から、敵艦を選択する(デバッグ用)
-		private int EUIndex(string name) {
-			return EnemyList
 				.Select((c, i) => new { Content = c, Index = i })
 				.Where(pair => pair.Content == name)
 				.Select(pair => pair.Index)
@@ -262,11 +214,6 @@ namespace AWSK.ViewModels
 			}
 			return Simulator.CalcBAURange(bauData.Weapon[bauIndex[index]]);
 		}
-		// 敵艦隊の制空値を返す
-		private int GetEnemyUnitAAV() {
-			var enemyData = GetEnemyData();
-			return Simulator.CalcAntiAirValue(enemyData, enemyData.GetSlotData(), true);
-		}
 		// 基地航空隊の制空値変更処理
 		private void ReCalcBasedAirUnit1AAV() {
 			int aav1 = GetBasedAirUnitAAV(0);
@@ -286,12 +233,327 @@ namespace AWSK.ViewModels
 			int range = GetBasedAirUnitRange(2);
 			BasedAirUnit3AAV.Value = $"制空値：{aav1}({Simulator.JudgeAirWarStatusStr(aav1, aav2)})　戦闘行動半径：{range}";
 		}
+		// 基地航空隊の読み込み処理
+		private void LoadBasedAirUnit() {
+			var ofd = new Microsoft.Win32.OpenFileDialog() {
+				FileName = "bau_data.bau",
+				Filter = "基地航空隊情報(*.bau)|*.bau|すべてのファイル(*.*)|*.*",
+				Title = "読み込むのファイルを選択"
+			};
+			if ((bool)ofd.ShowDialog()) {
+				try {
+					// ファイルを読み込み
+					using (var sr = new System.IO.StreamReader(ofd.FileName)) {
+						// テキストとして読み込んでパース
+						string output = sr.ReadToEnd();
+						var bauData = new BasedAirUnitData(output);
+						#region 基地航空隊の情報を初期化
+						BasedAirUnit1Mode.Value = 0;
+						BasedAirUnit2Mode.Value = 0;
+						BasedAirUnit3Mode.Value = 0;
+						BasedAirUnitIndex11.Value = 0;
+						BasedAirUnitIndex12.Value = 0;
+						BasedAirUnitIndex13.Value = 0;
+						BasedAirUnitIndex14.Value = 0;
+						BasedAirUnitIndex21.Value = 0;
+						BasedAirUnitIndex22.Value = 0;
+						BasedAirUnitIndex23.Value = 0;
+						BasedAirUnitIndex24.Value = 0;
+						BasedAirUnitIndex31.Value = 0;
+						BasedAirUnitIndex32.Value = 0;
+						BasedAirUnitIndex33.Value = 0;
+						BasedAirUnitIndex34.Value = 0;
+						BasedAirUnitMas11.Value = 0;
+						BasedAirUnitMas12.Value = 0;
+						BasedAirUnitMas13.Value = 0;
+						BasedAirUnitMas14.Value = 0;
+						BasedAirUnitMas21.Value = 0;
+						BasedAirUnitMas22.Value = 0;
+						BasedAirUnitMas23.Value = 0;
+						BasedAirUnitMas24.Value = 0;
+						BasedAirUnitMas31.Value = 0;
+						BasedAirUnitMas32.Value = 0;
+						BasedAirUnitMas33.Value = 0;
+						BasedAirUnitMas34.Value = 0;
+						BasedAirUnitRf11.Value = 0;
+						BasedAirUnitRf12.Value = 0;
+						BasedAirUnitRf13.Value = 0;
+						BasedAirUnitRf14.Value = 0;
+						BasedAirUnitRf21.Value = 0;
+						BasedAirUnitRf22.Value = 0;
+						BasedAirUnitRf23.Value = 0;
+						BasedAirUnitRf24.Value = 0;
+						BasedAirUnitRf31.Value = 0;
+						BasedAirUnitRf32.Value = 0;
+						BasedAirUnitRf33.Value = 0;
+						BasedAirUnitRf34.Value = 0;
+						#endregion
+						// 基地航空隊の情報を書き込む
+						for (int ui = 0; ui < bauData.Weapon.Count; ++ui) {
+							// 出撃回数
+							int count = bauData.SallyCount[ui];
+							switch (ui) {
+							case 0:
+								BasedAirUnit1Mode.Value = count;
+								break;
+							case 1:
+								BasedAirUnit2Mode.Value = count;
+								break;
+							case 2:
+								BasedAirUnit3Mode.Value = count;
+								break;
+							}
+							// 装備情報
+							var weaponList = bauData.Weapon[ui];
+							for (int wi = 0; wi < weaponList.Count; ++wi) {
+								var weapon = weaponList[wi];
+								int index = BasedAirUnitList.IndexOf(weapon.Name);
+								int mas = weapon.Mas;
+								int rf = weapon.Rf;
+								switch ((ui + 1) * 10 + (wi + 1)) {
+								case 11:
+									BasedAirUnitIndex11.Value = index;
+									BasedAirUnitMas11.Value = mas;
+									BasedAirUnitRf11.Value = rf;
+									break;
+								case 12:
+									BasedAirUnitIndex12.Value = index;
+									BasedAirUnitMas12.Value = mas;
+									BasedAirUnitRf12.Value = rf;
+									break;
+								case 13:
+									BasedAirUnitIndex13.Value = index;
+									BasedAirUnitMas13.Value = mas;
+									BasedAirUnitRf13.Value = rf;
+									break;
+								case 14:
+									BasedAirUnitIndex14.Value = index;
+									BasedAirUnitMas14.Value = mas;
+									BasedAirUnitRf14.Value = rf;
+									break;
+								case 21:
+									BasedAirUnitIndex21.Value = index;
+									BasedAirUnitMas21.Value = mas;
+									BasedAirUnitRf21.Value = rf;
+									break;
+								case 22:
+									BasedAirUnitIndex22.Value = index;
+									BasedAirUnitMas22.Value = mas;
+									BasedAirUnitRf22.Value = rf;
+									break;
+								case 23:
+									BasedAirUnitIndex23.Value = index;
+									BasedAirUnitMas23.Value = mas;
+									BasedAirUnitRf23.Value = rf;
+									break;
+								case 24:
+									BasedAirUnitIndex24.Value = index;
+									BasedAirUnitMas24.Value = mas;
+									BasedAirUnitRf24.Value = rf;
+									break;
+								case 31:
+									BasedAirUnitIndex31.Value = index;
+									BasedAirUnitMas31.Value = mas;
+									BasedAirUnitRf31.Value = rf;
+									break;
+								case 32:
+									BasedAirUnitIndex32.Value = index;
+									BasedAirUnitMas32.Value = mas;
+									BasedAirUnitRf32.Value = rf;
+									break;
+								case 33:
+									BasedAirUnitIndex33.Value = index;
+									BasedAirUnitMas33.Value = mas;
+									BasedAirUnitRf33.Value = rf;
+									break;
+								case 34:
+									BasedAirUnitIndex34.Value = index;
+									BasedAirUnitMas34.Value = mas;
+									BasedAirUnitRf34.Value = rf;
+									break;
+								}
+								/*// 対象とするプロパティ名を生成
+								string widx = $"BasedAirUnitIndex{ui + 1}{wi + 1}";
+								string wmas = $"BasedAirUnitMas{ui + 1}{wi + 1}";
+								string wrf = $"BasedAirUnitRf{ui + 1}{wi + 1}";
+								// プロパティ情報の取得
+								var widxProperty = typeof(MainViewModel).GetProperty(widx).PropertyType.GetProperty("Value");
+								var wmasProperty = typeof(MainViewModel).GetProperty(wmas).PropertyType.GetProperty("Value");
+								var wrfProperty = typeof(MainViewModel).GetProperty(wrf).PropertyType.GetProperty("Value");
+								// インスタンスに値を設定
+								widxProperty.SetValue(this, index);
+								wmasProperty.SetValue(this, mas);
+								wrfProperty.SetValue(this, rf);*/
+							}
+						}
+					}
+				} catch (Exception e) {
+					Console.WriteLine(e.ToString());
+					MessageBox.Show("ファイルを開けませんでした", "AWSK");
+				}
+			}
+		}
+		// 基地航空隊の保存処理
+		private void SaveBasedAirUnit() {
+			var sfd = new Microsoft.Win32.SaveFileDialog() {
+				FileName = "bau_data.bau",
+				Filter = "基地航空隊情報(*.bau)|*.bau|すべてのファイル(*.*)|*.*",
+				Title = "保存先のファイルを選択"
+			};
+			if ((bool)sfd.ShowDialog()) {
+				try {
+					string output = GetBasedAirUnitData().GetJsonData();
+					using (var sw = new System.IO.StreamWriter(sfd.FileName)) {
+						sw.Write(output);
+					}
+				} catch (Exception e) {
+					Console.WriteLine(e.ToString());
+					MessageBox.Show("ファイルに保存できませんでした", "AWSK");
+				}
+			}
+		}
+		#endregion
+		#region メソッド(敵艦隊用)
+		// 敵艦隊のデータを取得
+		private FleetData GetEnemyData() {
+			var fleetData = new FleetData();
+			// 艦隊データを格納するインスタンスを作成
+			var kammusuList = new List<KammusuData>();
+			// 準備
+			var enemyUnitIndex = new[] {
+				EnemyUnitIndex1.Value, EnemyUnitIndex2.Value, EnemyUnitIndex3.Value,
+				EnemyUnitIndex4.Value, EnemyUnitIndex5.Value, EnemyUnitIndex6.Value,
+				};
+			// 作成
+			foreach (int enemyIndex in enemyUnitIndex) {
+				// 「なし」が選択されている敵艦は無視する
+				if (enemyIndex == 0)
+					continue;
+				// idを算出する
+				int enemyId = enemyIdList[enemyIndex];
+				// idから敵艦情報を得る
+				var enemy = DataStore.KammusuDataById(enemyId, true);
+				// 追加
+				kammusuList.Add(enemy);
+			}
+			// インスタンスを代入
+			if (kammusuList.Count > 0)
+				fleetData.Kammusu.Add(kammusuList);
+			return fleetData;
+		}
+		// 艦名から、敵艦を選択する(デバッグ用)
+		private int EUIndex(string name) {
+			return EnemyList
+				.Select((c, i) => new { Content = c, Index = i })
+				.Where(pair => pair.Content == name)
+				.Select(pair => pair.Index)
+				.First();
+		}
+		// 敵艦隊の制空値を返す
+		private int GetEnemyUnitAAV() {
+			var enemyData = GetEnemyData();
+			return Simulator.CalcAntiAirValue(enemyData, enemyData.GetSlotData(), true);
+		}
 		// 敵艦隊の制空値変更処理
 		private void ReCalcEnemyUnitAAV() {
 			EnemyUnitAAV.Value = $"制空値：{GetEnemyUnitAAV()}";
 			ReCalcBasedAirUnit1AAV();
 			ReCalcBasedAirUnit2AAV();
 			ReCalcBasedAirUnit3AAV();
+		}
+		// 敵艦隊の読み込み処理
+		private void LoadEnemyUnit() {
+			var ofd = new Microsoft.Win32.OpenFileDialog() {
+				FileName = "enemy_data.enemy",
+				Filter = "敵艦隊情報(*.enemy)|*.enemy|すべてのファイル(*.*)|*.*",
+				Title = "読み込むのファイルを選択"
+			};
+			if ((bool)ofd.ShowDialog()) {
+				try {
+					// ファイルを読み込み
+					using (var sr = new System.IO.StreamReader(ofd.FileName)) {
+						// テキストとして読み込んでパース
+						string output = sr.ReadToEnd();
+						var enemyData = new FleetData(output);
+						// 敵艦隊の情報を初期化
+						EnemyUnitIndex1.Value = 0;
+						EnemyUnitIndex2.Value = 0;
+						EnemyUnitIndex3.Value = 0;
+						EnemyUnitIndex4.Value = 0;
+						EnemyUnitIndex5.Value = 0;
+						EnemyUnitIndex6.Value = 0;
+						// IDとリストの表示位置との対応表を入手
+						var enemyNameList = DataStore.EnemyNameList().ToList().Select(pair => pair.Key).ToList();
+						// 敵艦隊の情報を書き込む
+						if (enemyData.Kammusu.Count > 0) {
+							int i = 0;
+							foreach (var kammusu in enemyData.Kammusu[0]) {
+								int index = enemyNameList.IndexOf(kammusu.Id);
+								switch (i) {
+								case 0:
+									EnemyUnitIndex1.Value = index;
+									break;
+								case 1:
+									EnemyUnitIndex2.Value = index;
+									break;
+								case 2:
+									EnemyUnitIndex3.Value = index;
+									break;
+								case 3:
+									EnemyUnitIndex4.Value = index;
+									break;
+								case 4:
+									EnemyUnitIndex5.Value = index;
+									break;
+								case 5:
+									EnemyUnitIndex6.Value = index;
+									break;
+								}
+								++i;
+							}
+						}
+					}
+				} catch (Exception e) {
+					Console.WriteLine(e.ToString());
+					MessageBox.Show("ファイルを開けませんでした", "AWSK");
+				}
+			}
+		}
+		// 敵艦隊の保存処理
+		private void SaveEnemyUnit() {
+			var sfd = new Microsoft.Win32.SaveFileDialog() {
+				FileName = "enemy_data.enemy",
+				Filter = "敵艦隊情報(*.enemy)|*.enemy|すべてのファイル(*.*)|*.*",
+				Title = "保存先のファイルを選択"
+			};
+			if ((bool)sfd.ShowDialog()) {
+				try {
+					string output = GetEnemyData().GetJsonData();
+					using (var sw = new System.IO.StreamWriter(sfd.FileName)) {
+						sw.Write(output);
+					}
+				} catch (Exception e) {
+					Console.WriteLine(e.ToString());
+					MessageBox.Show("ファイルに保存できませんでした", "AWSK");
+				}
+			}
+		}
+		#endregion
+		// その他初期化用コード
+		private async void Initialize() {
+			// データベースの初期化
+			var status = await DataStore.Initialize();
+			switch (status) {
+			case DataStoreStatus.Exist:
+				break;
+			case DataStoreStatus.Success:
+				MessageBox.Show("ダウンロードに成功しました。", "AWSK");
+				break;
+			case DataStoreStatus.Failed:
+				MessageBox.Show("ダウンロードに失敗しました。", "AWSK");
+				CloseWindow.Value = true;
+				return;
+			}
 		}
 
 		// クリップボードからインポート
@@ -344,7 +606,7 @@ namespace AWSK.ViewModels
 		public MainViewModel() {
 			// その他初期化
 			Initialize();
-			// プロパティ・コマンドを設定
+			// プロパティを設定
 			BasedAirUnit1Flg = BasedAirUnit1Mode.Select(x => x != 0).ToReadOnlyReactiveProperty();
 			BasedAirUnit2Flg = BasedAirUnit2Mode.Select(x => x != 0).ToReadOnlyReactiveProperty();
 			BasedAirUnit3Flg = BasedAirUnit3Mode.Select(x => x != 0).ToReadOnlyReactiveProperty();
@@ -386,6 +648,7 @@ namespace AWSK.ViewModels
 				BasedAirUnitList = oc.ToReadOnlyReactiveCollection();
 			}
 			#endregion
+			// コマンドを設定
 			#region 設定変更により制空値を自動計算する処理
 			BasedAirUnitIndex11.Subscribe(_ => ReCalcBasedAirUnit1AAV());
 			BasedAirUnitIndex12.Subscribe(_ => ReCalcBasedAirUnit1AAV());
@@ -434,84 +697,11 @@ namespace AWSK.ViewModels
 				.CombineLatest(x => x.Any(y => y)).ToReactiveCommand();
 			RunSimulationCommand.Subscribe(RunSimulation);
 			UpdateDatabaseCommand.Subscribe(UpdateDatabase);
-			ShowEnemyUnitCommand.Subscribe(_ => {
-				MessageBox.Show("敵編成：\n" + GetEnemyData().ToString(), "AWSK");
-			});
-			LoadEnemyUnitCommand.Subscribe(_ => {
-				var ofd = new Microsoft.Win32.OpenFileDialog(){
-					FileName = "enemy_data.enemy",
-					Filter = "敵艦隊情報(*.enemy)|*.enemy|すべてのファイル(*.*)|*.*",
-					Title = "読み込むのファイルを選択"
-				};
-				if ((bool)ofd.ShowDialog()) {
-					try {
-						// ファイルを読み込み
-						using (var sr = new System.IO.StreamReader(ofd.FileName)) {
-							// テキストとして読み込んでパース
-							string output = sr.ReadToEnd();
-							var enemyData = new FleetData(output);
-							// 敵艦隊の情報を初期化
-							EnemyUnitIndex1.Value = 0;
-							EnemyUnitIndex2.Value = 0;
-							EnemyUnitIndex3.Value = 0;
-							EnemyUnitIndex4.Value = 0;
-							EnemyUnitIndex5.Value = 0;
-							EnemyUnitIndex6.Value = 0;
-							// IDとリストの表示位置との対応表を入手
-							var enemyNameList = DataStore.EnemyNameList().ToList().Select(pair => pair.Key).ToList();
-							// 敵艦隊の情報を書き込む
-							if(enemyData.Kammusu.Count > 0) {
-								int i = 0;
-								foreach(var kammusu in enemyData.Kammusu[0]) {
-									int index = enemyNameList.IndexOf(kammusu.Id);
-									switch (i) {
-									case 0:
-										EnemyUnitIndex1.Value = index;
-										break;
-									case 1:
-										EnemyUnitIndex2.Value = index;
-										break;
-									case 2:
-										EnemyUnitIndex3.Value = index;
-										break;
-									case 3:
-										EnemyUnitIndex4.Value = index;
-										break;
-									case 4:
-										EnemyUnitIndex5.Value = index;
-										break;
-									case 5:
-										EnemyUnitIndex6.Value = index;
-										break;
-									}
-									++i;
-								}
-							}
-						}
-					} catch (Exception e) {
-						Console.WriteLine(e.ToString());
-						MessageBox.Show("ファイルを開けませんでした", "AWSK");
-					}
-				}
-			});
-			SaveEnemyUnitCommand.Subscribe(_ => {
-				var sfd = new Microsoft.Win32.SaveFileDialog(){
-					FileName = "enemy_data.enemy",
-					Filter = "敵艦隊情報(*.enemy)|*.enemy|すべてのファイル(*.*)|*.*",
-					Title = "保存先のファイルを選択"
-				};
-				if ((bool)sfd.ShowDialog()) {
-					try {
-						string output = GetEnemyData().GetJsonData();
-						using (var sw = new System.IO.StreamWriter(sfd.FileName)) {
-							sw.Write(output);
-						}
-					}catch(Exception e) {
-						Console.WriteLine(e.ToString());
-						MessageBox.Show("ファイルに保存できませんでした", "AWSK");
-					}
-				}
-			});
+			ShowEnemyUnitCommand.Subscribe(_ => { MessageBox.Show("敵編成：\n" + GetEnemyData().ToString(), "AWSK"); });
+			LoadEnemyUnitCommand.Subscribe(_ => LoadEnemyUnit());
+			SaveEnemyUnitCommand.Subscribe(_ => SaveEnemyUnit());
+			LoadBasedAirUnitCommand.Subscribe(_ => LoadBasedAirUnit());
+			SaveBasedAirUnitCommand.Subscribe(_ => SaveBasedAirUnit());
 		}
 	}
 }
